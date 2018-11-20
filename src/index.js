7f6b526235e48e0e33afac81c4b5129d30910c2d
Vitaly Yerofeyevsky <flytaly@gmail.com>
2018-11-20 21:37:19 +0300

    Move from using dotenv to dotenv-cli. Create additional .env file for dev stage.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-require('dotenv').config();
-
 const createServer = require('./server');
 const db = require('./bind-prisma');
 
 const server = createServer(db);
-
 server.start(({ port }) => console.log(
     `Server started, listening on port ${port} for incoming requests.`,
 ));
