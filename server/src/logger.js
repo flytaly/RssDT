061722f3a6efcd58f68ac19219f3585450855fc6
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-01-12 00:24:37 +0300

    Save logs to files

--- server/src/logger.js
+++ server/src/logger.js
@@ -1,5 +1,28 @@
 const pino = require('pino');
+const childProcess = require('child_process');
+const stream = require('stream');
 
-const logger = pino();
+const cwd = process.cwd();
+const { env } = process;
+const logPath = `${cwd}/log`;
+
+// Create a stream where the logs will be written
+const logThrough = new stream.PassThrough();
+const logger = pino({ name: 'fd' }, logThrough);
+
+// Log to multiple files using a separate process
+const child = childProcess.spawn(process.execPath, [
+    require.resolve('pino-tee'),
+    'warn', `${logPath}/warn.log`,
+    'info', `${logPath}/info.log`,
+    'error', `${logPath}/error.log`,
+    'fatal', `${logPath}/fatal.log`,
+], { cwd, env });
+
+logThrough.pipe(child.stdin);
+
+if (env.NODE_ENV === 'development') {
+    logThrough.pipe(process.stdout);
+}
 
 module.exports = logger;
