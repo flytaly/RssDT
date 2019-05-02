c48cd83eb396e2278fbb04904f129e0098f0b865
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-05-02 14:48:05 +0300

    Add myFeedItems query

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -9,6 +9,17 @@ const Queries = {
 
         return ctx.db.query.userFeeds({ where: { user: { id: user.id } } }, info);
     },
+    async myFeedItems(parent, args, ctx, info) {
+        const { user } = ctx.request;
+        const { feedId: id } = args;
+        if (!await ctx.db.exists.UserFeed({
+            user: { id: user.id },
+            feed: { id },
+        })) {
+            return [];
+        }
+        return ctx.db.query.feedItems({ where: { feed: { id } } }, info);
+    },
 
 };
 
