8193c4b1fa209826076cc372bb87f4f37e81103e
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-06-15 21:44:19 +0300

    Update next-css and add hack to prevent error

--- client/next.config.js
+++ client/next.config.js
@@ -1,18 +1,28 @@
 const withCSS = require('@zeit/next-css');
 const withReactSvg = require('next-react-svg');
-const withPlugins = require('next-compose-plugins');
-// const path = require('path');
 
-module.exports = withPlugins([
-    [withReactSvg,
-        {
-            include: `${__dirname}/static`,
-            webpack(config, options) {
-                return config;
-            },
-        },
-    ],
-    withCSS,
-], {
+// https://github.com/zeit/next-plugins/issues/392#issuecomment-475845330
+function HACK_removeMinimizeOptionFromCssLoaders(config) {
+    console.warn(
+        'HACK: Removing `minimize` option from `css-loader` entries in Webpack config',
+    );
+    config.module.rules.forEach((rule) => {
+        if (Array.isArray(rule.use)) {
+            rule.use.forEach((u) => {
+                if (u.loader === 'css-loader' && u.options) {
+                    delete u.options.minimize;
+                }
+            });
+        }
+    });
+}
+
+
+module.exports = withCSS(withReactSvg({
+    include: `${__dirname}/static`,
     target: 'serverless',
-});
+    webpack(config) {
+        HACK_removeMinimizeOptionFromCssLoaders(config);
+        return config;
+    },
+}));
