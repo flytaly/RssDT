34125dda52a9ffbeedc77841c2797a28711469ee
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-06-28 05:07:17 +0300

    Add validations to updateMyInfo mutation args

--- server/src/middlewares/shield.js
+++ server/src/middlewares/shield.js
@@ -1,5 +1,20 @@
+/* eslint-disable no-shadow */
 const moment = require('moment-timezone');
 const { shield, inputRule } = require('graphql-shield');
+const yup = require('yup');
+const share = require('../mail-sender/share');
+
+const shareNames = new Set(share.map(({ id }) => id));
+
+const valids = {
+    timeZone: yup.string()
+        .test('TimeZone', 'Not valid argument: timeZone', (value) => {
+            if (!value || moment.tz.zone(value)) return true;
+            return false;
+        }),
+    dailyDigestHour: yup.number().integer().max(23).min(0),
+    locale: yup.string().max(50),
+};
 
 const addFeedValidation = inputRule(yup => yup.object({
     email: yup
@@ -10,16 +25,37 @@ const addFeedValidation = inputRule(yup => yup.object({
         .string()
         .url('Not valid argument: feedUrl')
         .required('Not valid argument: feedUrl'),
-    timeZone: yup
-        .string()
-        .test('TimeZone', 'Not valid argument: timeZone', (value) => {
-            if (!value || moment.tz.zone(value)) return true;
-            return false;
-        }),
+    timeZone: valids.timeZone,
+    dailyDigestHour: valids.dailyDigestHour,
+    locale: valids.locale,
+}));
+
+const updateMyInfoValidation = inputRule(yup => yup.object({
+    data: yup.object({
+        timeZone: valids.timeZone,
+        filterShare: yup
+            .array()
+            .test('filterShare', 'Not valid argument: filterShare', (value) => {
+                if (!value) return true;
+                return value.every(name => shareNames.has(name));
+            }),
+        locale: valids.locale,
+        dailyDigestHour: valids.dailyDigestHour,
+    })
+        .required(),
+}));
+
+const setPasswordValidation = inputRule(yup => yup.object({
+    token: yup.string().required(),
+    password: yup.string().min(8).required(),
 }));
 
 module.exports = shield({
     Mutation: {
         addFeed: addFeedValidation,
+        setPassword: setPasswordValidation,
+        updateMyInfo: updateMyInfoValidation,
     },
+}, {
+    debug: true,
 });
--- server/src/resolvers/mutations/updateMyInfo.js
+++ server/src/resolvers/mutations/updateMyInfo.js
@@ -1,20 +1,8 @@
-const moment = require('moment-timezone');
-const share = require('../../mail-sender/share');
-
-const shareNames = new Set(share.map(({ id }) => id));
-
 async function updateMyInfo(parent, args, ctx, info) {
     const { user: { id } } = ctx.request;
     const { data } = args;
-    if (data.filterShare) {
-        if (!data.filterShare.every(name => shareNames.has(name))) {
-            return new Error('Not valid argument: filterShare');
-        }
-        data.filterShare = { set: data.filterShare };
-    }
-    if (data.timeZone && !moment.tz.zone(data.timeZone)) {
-        return new Error('Not valid argument: timeZone');
-    }
+    const { filterShare } = data;
+    if (filterShare) { data.filterShare = { set: filterShare }; }
     const User = await ctx.db.mutation.updateUser({ data, where: { id } }, info);
 
     return User;
