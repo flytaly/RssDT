51e92a0149988830f9022c93d1cccea83c732478
Vitaly Yerofeyevsky <flytaly@gmail.com>
2019-08-01 23:42:13 +0300

    Fix signOut cookie for development

--- server/src/resolvers/mutations/signOut.js
+++ server/src/resolvers/mutations/signOut.js
@@ -1,8 +1,10 @@
+const isDev = process.env.NODE_ENV === 'development';
+
 async function signOut(parent, args, ctx) {
     const domain = process.env.COOKIE_DOMAIN;
     ctx.response.clearCookie('token', {
         httpOnly: true,
-        secure: true,
+        secure: !isDev,
         ...(domain ? { domain } : {}),
     });
     return { message: 'OK' };
