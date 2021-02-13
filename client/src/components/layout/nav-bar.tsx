0d0177b7f80c4dce6a3dbbfb92e940c1b1d7e0c3
Vitaly Yerofeyevsky <flytaly@gmail.com>
2021-02-13 21:54:15 +0300

    Update icons in the navbar

--- client/src/components/layout/nav-bar.tsx
+++ client/src/components/layout/nav-bar.tsx
@@ -1,21 +1,39 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import ProfileIcon from '../../../public/static/user-circle-solid.svg';
 import { useMeQuery } from '../../generated/graphql';
 import { isServer } from '../../utils/is-server';
+import HelpCircle from '../../../public/static/help-circle.svg';
+import SettingsIcon from '../../../public/static/settings.svg';
 
 interface NavLinkProps {
   href: string;
   title: string;
+  icon?: React.ReactNode;
   className?: string;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ className = '', href, title }) => (
-  <li className={className}>
-    <Link href={href}>
-      <a className="hover-underline-link">{title}</a>
-    </Link>
-  </li>
-);
+const NavLink: React.FC<NavLinkProps> = ({ className = '', href, title, icon }) => {
+  const router = useRouter();
+  return (
+    <li className={className}>
+      <Link href={href}>
+        {icon ? (
+          <button
+            type="button"
+            className="icon-btn flex items-center"
+            title={title}
+            onClick={() => router.push(href)}
+          >
+            {icon}
+          </button>
+        ) : (
+          <a className="hover-underline-link">{title}</a>
+        )}
+      </Link>
+    </li>
+  );
+};
 const SubNavLink: React.FC<NavLinkProps> = ({ className = '', href, title }) => (
   <li className={className}>
     <Link href={href}>
@@ -28,20 +46,23 @@ const NavBar = () => {
   const { data, loading } = useMeQuery({ skip: isServer() });
   let content: JSX.Element | null = null;
 
+  const help = <HelpCircle className="h-5" />;
   if (loading) content = <div />;
   else if (data?.me) {
     content = (
       <>
         <NavLink href="/feed" title="Reader" />
         <NavLink href="/manage" title="Manage" />
-        <NavLink className="hidden sm:block" href="/settings" title="Settings" />
+        {/* <NavLink className="hidden sm:block" href="/settings" title="Settings" /> */}
+        <NavLink href="/settings" title="Settings" icon={<SettingsIcon className="h-5" />} />
+        <NavLink href="/help" title="Help" icon={help} />
         <li>
           <div className="group relative flex">
             <button type="button" className="icon-btn ">
               <ProfileIcon className="h-5" />
             </button>
             <ul className="hidden group-hover:block absolute top-full right-0 bg-primary-light bg-opacity-80 z-40 rounded-sm shadow-message hover:shadow-message-darker">
-              <SubNavLink className="block sm:hidden" href="/settings" title="Settings" />
+              {/* <SubNavLink className="block sm:hidden" href="/settings" title="Settings" /> */}
               <SubNavLink href="/logout" title="Logout" />
             </ul>
           </div>
@@ -51,16 +72,9 @@ const NavBar = () => {
   } else {
     content = (
       <>
-        <li>
-          <Link href="/login">
-            <a className="hover-underline-link">Log in</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/register">
-            <a className="hover-underline-link">Register</a>
-          </Link>
-        </li>
+        <NavLink href="/login" title="Log in" />
+        <NavLink href="/register" title="Register" />
+        <NavLink href="/help" title="Help" icon={help} />
       </>
     );
   }
