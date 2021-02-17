2ee327893ceb9813e0c0b4f1a2560a28467e77b9
Vitaly Yerofeyevsky <flytaly@gmail.com>
2021-02-17 22:12:29 +0300

    Set cookie to trust proxy

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ import { redis } from './redis';
 
 const entry = async () => {
   const app = express();
-  app.set('proxy', 1);
+  app.set('trust proxy', 1);
   app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
   initSession(app, redis);
