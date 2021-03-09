21114b95a44f2e211886b6c835d63c28935e62d0
Vitaly Yerofeyevsky <flytaly@gmail.com>
2021-03-09 22:10:58 +0300

    Highlight input on hover

--- client/src/components/forms/input-underline.tsx
+++ client/src/components/forms/input-underline.tsx
@@ -8,7 +8,7 @@ export type InputUnderlineProps = React.DetailedHTMLProps<
 const InputUnderline: React.FC<InputUnderlineProps> = ({ className, ...props }) => {
   return (
     <input
-      className={`bg-transparent border-b-2 border-gray-500 focus:border-primary placeholder-gray-700 ${className}`}
+      className={`bg-transparent border-b-2 border-gray-500 focus:border-primary placeholder-gray-700 focus:bg-white hover:bg-white ${className}`}
       {...props}
     />
   );
