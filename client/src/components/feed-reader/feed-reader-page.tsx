dbd551d2651a5f979ee47af02a8eae45c2302399
Vitaly Yerofeyevsky <flytaly@gmail.com>
2021-03-19 02:52:55 +0300

    Fix layout shift during feed loading

--- client/src/components/feed-reader/feed-reader-page.tsx
+++ client/src/components/feed-reader/feed-reader-page.tsx
@@ -1,11 +1,20 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '../layout/layout';
 import FeedNavBar from '../main-card/feed-nav-bar';
 import MainCard from '../main-card/main-card';
 import FeedReader from './feed-reader';
 
 const FeedReaderPage: NextPage<{ id?: number }> = ({ id }) => {
+  // Add scroll to prevent layout shift while loading feed items
+  useEffect(() => {
+    const initial = document.body.style.overflowY;
+    document.body.style.overflowY = 'scroll';
+    return () => {
+      document.body.style.overflowY = initial;
+    };
+  }, []);
+
   return (
     <Layout>
       <MainCard big onlyWithVerifiedEmail>
