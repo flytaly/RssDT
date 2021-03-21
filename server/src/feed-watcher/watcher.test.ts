ea5d40a84cd745fdc754519b7dccd96e17efc523
Vitaly Yerofeyevsky <flytaly@gmail.com>
2021-03-21 22:47:43 +0300

    Watcher add notification test

--- server/src/feed-watcher/watcher.test.ts
+++ server/src/feed-watcher/watcher.test.ts
@@ -1,7 +1,10 @@
 import faker from 'faker';
+import { RedisPubSub } from 'graphql-redis-subscriptions';
 import moment from 'moment';
 import { throttleMultiplier } from '../constants';
 import { buildAndSendDigests } from '../digests/build-and-send';
+import { createRedis } from '../redis';
+import { NewItemsPayload, PubSubTopics } from '../resolvers/common/pubSubTopics';
 import Watcher from './watcher';
 import * as watcherUtils from './watcher-utils';
 
@@ -9,7 +12,7 @@ const updateFeedData = watcherUtils.updateFeedData as jest.Mock;
 const getFeedsToUpdate = watcherUtils.getFeedsToUpdate as jest.Mock;
 
 jest.mock('./watcher-utils.ts', () => ({
-  updateFeedData: jest.fn(async () => [0, 0]),
+  updateFeedData: jest.fn(async () => []),
   getFeedsToUpdate: jest.fn(async () => []),
 }));
 jest.mock('../digests/build-and-send.ts', () => ({
@@ -36,15 +39,6 @@ describe('Feed watcher schedule', () => {
     await feedWatcher.cancel();
     expect(feedWatcher.getNextUpdateTime()).toBeNull();
   });
-
-  // test('should call update every fixed amount of time', async () => {
-  //     const feedWatcher = new Watcher({ cron: '*/1 * * * * *' }); // update every second
-  //     feedWatcher.update = jest.fn(async () => {});
-  //     feedWatcher.start();
-  //     await new Promise((resolve) => setTimeout(resolve, 2000));
-  //     feedWatcher.cancel();
-  //     expect(feedWatcher.update).toHaveBeenCalledTimes(2);
-  // });
 });
 
 function generatePartialFeeds(num: number, sinceLastUpdateTs = 1000 * 60 * 5) {
@@ -52,7 +46,7 @@ function generatePartialFeeds(num: number, sinceLastUpdateTs = 1000 * 60 * 5) {
     const ts = new Date(tsNow - sinceLastUpdateTs);
     const feed: watcherUtils.PartialFeed = {
       url: `${faker.internet.url()}/feed.rss`,
-      id: index,
+      id: index + 1,
       throttled: 0,
       lastSuccessfulUpd: ts,
       lastUpdAttempt: ts,
@@ -62,22 +56,51 @@ function generatePartialFeeds(num: number, sinceLastUpdateTs = 1000 * 60 * 5) {
 }
 
 describe('Watcher Update', () => {
+  const pubsub = new RedisPubSub({ subscriber: createRedis() });
   const feedWatcher = new Watcher();
   const feeds = generatePartialFeeds(6, throttleMultiplier + 100);
-  feeds[0].throttled = 1;
-  feeds[1].throttled = 2;
-  feeds[2].throttled = 3;
-  getFeedsToUpdate.mockImplementation(async () => feeds);
-  afterAll(async () => feedWatcher.cancel());
+
+  beforeAll(() => {
+    getFeedsToUpdate.mockImplementation(async () => feeds);
+    updateFeedData.mockImplementation(async () => [true, 22]);
+  });
+
+  afterAll(async () => {
+    await feedWatcher.cancel();
+    await pubsub.close();
+  });
   test('should call updateFeedData on not throttled feed', async () => {
+    feeds[0].throttled = 1;
+    feeds[1].throttled = 2;
+    feeds[2].throttled = 3;
+
     await feedWatcher.update();
     expect(getFeedsToUpdate).toHaveBeenCalled();
     expect(updateFeedData).toHaveBeenCalledTimes(4);
     expect(updateFeedData).toHaveBeenCalledWith(feeds[0].url);
     expect(updateFeedData).not.toHaveBeenCalledWith(feeds[1].url);
+
+    feeds.forEach((f) => {
+      f.throttled = 0;
+    });
   });
 
   test('should call buildAndSendDigests after updating', async () => {
     feeds.forEach(({ id }) => expect(buildAndSendDigests).toHaveBeenCalledWith(id));
+  });
+
+  test('should notify about new items', async (done) => {
+    await pubsub.subscribe(PubSubTopics.newItems, (payload: NewItemsPayload) => {
+      expect(payload).not.toHaveProperty('1');
+      expect(payload).toMatchObject(
+        feeds.slice(1).reduce((prev, f) => ({ ...prev, [f.id]: { count: f.id - 1 } }), {}),
+      );
+      done();
+    });
+
+    let newItems = 0;
+    updateFeedData.mockImplementation(async () => [true, newItems++]);
+
+    await feedWatcher.update();
   });
 });
