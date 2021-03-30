94d1eb8fa0dff7f0eca2611e21d18f7140ceadeb
Vitaly Yerofeyevsky <flytaly@gmail.com>
2021-03-30 21:08:56 +0300

    Split code

---
+++ client/src/components/feed-reader/feed-content.tsx
@@ -0,0 +1,83 @@
+import debounce from 'lodash.debounce';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import BarsIcon from '../../../public/static/bars.svg';
+import { useItemsCountUpdatedSubscription, UserFeed } from '../../generated/graphql';
+import { createUpdateOnNewItems } from '../../utils/update-unread-count';
+import FeedHeader from './feed-header';
+import FeedItems from './feed-items';
+import { useLocalState } from './reader-options';
+
+const FeedContent: React.FC<{
+  userFeed: UserFeed;
+  setSidebarModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({ userFeed, setSidebarModalOpen }) => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [readerOpts, setReaderOpts] = useLocalState();
+  const [searchFilter, setSearchFilter] = useState('');
+  const [hasNewItems, setHasNewItems] = useState(false);
+
+  useEffect(() => {
+    setShowSearch(false);
+    setSearchFilter('');
+    setHasNewItems(false);
+  }, [userFeed.id]);
+
+  useItemsCountUpdatedSubscription({
+    onSubscriptionData: createUpdateOnNewItems(userFeed?.feed.id, () => setHasNewItems(true)),
+  });
+
+  const toggleSearch = () => setShowSearch((s) => !s);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
+    setSearchFilter(event.target.value);
+
+  return (
+    <>
+      <div className="px-4 py-1 my-1">
+        <div className="flex items-center flex-wrap">
+          <button
+            type="button"
+            className="md:hidden icon-btn mr-4"
+            title="Open list of the feeds"
+            onClick={() => setSidebarModalOpen((prev) => !prev)}
+          >
+            <BarsIcon className="h-4 w-4" />
+          </button>
+          <FeedHeader
+            userFeed={userFeed}
+            readerOpts={readerOpts}
+            setReaderOpts={setReaderOpts}
+            toggleSearch={toggleSearch}
+          />
+        </div>
+        {showSearch ? (
+          <div className="mt-2">
+            <input
+              type="text"
+              maxLength={250}
+              placeholder="filter feed items by words in their titles"
+              className="text-sm shadow-message hover:shadow-message-darker w-full py-1 px-2 rounded-sm
+              "
+              onChange={debounce(handleSearch, 800)}
+            />
+            <div>
+              <Link href="/help#filter">
+                <a className="underline text-xs">search syntax</a>
+              </Link>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <FeedItems
+        feed={userFeed}
+        readerOpts={readerOpts}
+        filter={searchFilter}
+        showRefetchBtn={hasNewItems}
+        onRefetchEnd={() => setHasNewItems(false)}
+      />
+    </>
+  );
+};
+
+export default FeedContent;
--- client/src/components/feed-reader/feed-reader.tsx
+++ client/src/components/feed-reader/feed-reader.tsx
@@ -1,46 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import debounce from 'lodash.debounce';
-import Link from 'next/link';
-import BarsIcon from '../../../public/static/bars.svg';
-import {
-  useItemsCountUpdatedSubscription,
-  useMyFeedsQuery,
-  UserFeed,
-} from '../../generated/graphql';
+import React, { useState } from 'react';
+import { useMyFeedsQuery, UserFeed } from '../../generated/graphql';
 import AddFeedModal from '../modals/add-feed-modal';
 import ModalSidebar from '../modals/modal-sidebar';
-import FeedHeader from './feed-header';
-import FeedItems from './feed-items';
 import FeedSidebar from './feed-sidebar';
-import { useLocalState } from './reader-options';
-import { createUpdateOnNewItems } from '../../utils/update-unread-count';
+import FeedContent from './feed-content';
 
 const FeedReader: React.FC<{ id?: number }> = ({ id }) => {
   const [sidebarModalOpen, setSidebarModalOpen] = useState(false);
   const [addFeedModalOpen, setAddFeedModalOpen] = useState(false);
-  const [showSearch, setShowSearch] = useState(false);
   const { data, loading } = useMyFeedsQuery({ ssr: false });
-  const [readerOpts, setReaderOpts] = useLocalState();
-  const [searchFilter, setSearchFilter] = useState('');
-  const [hasNewItems, setHasNewItems] = useState(false);
-
-  useEffect(() => {
-    setShowSearch(false);
-    setSearchFilter('');
-    setHasNewItems(false);
-  }, [id]);
-
   const myFeeds = data?.myFeeds || ([] as UserFeed[]);
   const userFeed = id && !loading ? myFeeds.find((uf) => uf.id === id) : null;
-
-  useItemsCountUpdatedSubscription({
-    onSubscriptionData: createUpdateOnNewItems(userFeed?.feed.id, () => setHasNewItems(true)),
-  });
-
-  const toggleSearch = () => setShowSearch((s) => !s);
-
-  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
-    setSearchFilter(event.target.value);
 
   return (
     <section className="block md:reader-layout flex-grow bg-gray-200 limit-width">
@@ -62,52 +32,9 @@ const FeedReader: React.FC<{ id?: number }> = ({ id }) => {
           <div className="w-px h-full bg-gray-100 bg-opacity-50 group-hover:bg-opacity-100" />
         </button>
       </aside>
-      <div className="px-4 py-1 my-1">
-        <div className="flex items-center flex-wrap">
-          <button
-            type="button"
-            className="md:hidden icon-btn mr-4"
-            title="Open list of the feeds"
-            onClick={() => setSidebarModalOpen((prev) => !prev)}
-          >
-            <BarsIcon className="h-4 w-4" />
-          </button>
-          <FeedHeader
-            userFeed={userFeed}
-            readerOpts={readerOpts}
-            setReaderOpts={setReaderOpts}
-            toggleSearch={toggleSearch}
-          />
-        </div>
-        {showSearch ? (
-          <div className="mt-2">
-            <input
-              type="text"
-              maxLength={250}
-              placeholder="filter feed items by words in their titles"
-              className="text-sm shadow-message hover:shadow-message-darker w-full py-1 px-2 rounded-sm
-              "
-              onChange={debounce(handleSearch, 800)}
-            />
-            <div>
-              <Link href="/help#filter">
-                <a className="underline text-xs">search syntax</a>
-              </Link>
-            </div>
-          </div>
-        ) : null}
-      </div>
       {userFeed ? (
-        <FeedItems
-          feed={userFeed}
-          readerOpts={readerOpts}
-          filter={searchFilter}
-          showRefetchBtn={hasNewItems}
-          onRefetchEnd={() => setHasNewItems(false)}
-        />
-      ) : (
-        <div />
-      )}
+        <FeedContent userFeed={userFeed as UserFeed} setSidebarModalOpen={setSidebarModalOpen} />
+      ) : null}
       <ModalSidebar
         isOpen={sidebarModalOpen}
         closeModal={() => setSidebarModalOpen(false)}
