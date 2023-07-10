97f8c7375fab4d2cfc79cbd02e4002d3d72a7aac
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-07-11 02:42:19 +0300

    Add script to launch databases and mailcatcher

---
+++ db_and_mail.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Launch Redis, Postgres and Mailcatcher
+#
+
+CYAN='\033[1;36m'
+NC='\033[0m' # No ColorED='\033[0;31m'
+
+output="$(redis-cli ping)"
+if [[ $output != "PONG" ]]; then
+	echo "Launch redis ${CYAN}server${NC}"
+	redis-server &
+else
+	echo -e "${CYAN}Redis${NC} was already started"
+fi
+
+output="$(systemctl is-active postgresql)"
+if [[ $output != "active" ]]; then
+	echo "Launch ${CYAN}postgres${NC}"
+	systemctl start postgresql
+else
+	echo -e "${CYAN}Postgres${NC} was already started"
+fi
+
+mailPort=1025
+output="$(lsof -i:${mailPort})"
+if [[ -z $output ]]; then
+	echo "Launch ${CYAN}mailcatcher${NC}"
+	mailcatcher
+else
+	echo -e "${CYAN}Port ${mailPort}${NC} is already in use"
+fi
