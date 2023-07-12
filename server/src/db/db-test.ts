886334b0a613306ea8442d97e7efad902edc1938
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-07-12 17:03:01 +0300

    drizzle-orm: declare feed, item and enclosure

--- server/src/db/db-test.ts
+++ server/src/db/db-test.ts
@@ -1,10 +1,14 @@
 import '../dotenv.js';
 import { db } from './db.js';
-/* import { user } from './schema.js'; */
+import { user } from './schema.js';
 
 async function selectTest() {
-  /* console.log(await db.select().from(user)); */
-  console.log(await db.query.user.findMany({ with: { options: true } }));
+  /* return db.select().from(user) */
+  /* return db.query.user.findMany({ with: { options: true } }); */
+  return db.query.feed.findMany({
+    limit: 2,
+    with: { items: { limit: 2, with: { enclosures: true } } },
+  });
 }
 
-selectTest();
+selectTest().then((x) => console.log(JSON.stringify(x, null, 2)));
--- server/src/db/schema.ts
+++ server/src/db/schema.ts
@@ -53,3 +53,72 @@ export const options = pgTable('options', {
 
 export type User = InferModel<typeof user>;
 export type NewUser = InferModel<typeof user, 'insert'>;
+
+export const feed = pgTable('feed', {
+  id: serial('id').primaryKey(),
+  url: varchar('url', { length: 2048 }).unique().notNull(),
+  link: varchar('link', { length: 2048 }),
+  title: varchar('title', { length: 2000 }).default(''),
+  description: text('description').default(''),
+  language: varchar('language', { length: 50 }),
+  /** A link to the favicon from feed (provided by Atom feeds) */
+  favicon: varchar('favicon', { length: 2048 }),
+  /** A link to the favicon of the website */
+  siteFavicon: varchar('siteFavicon', { length: 2048 }),
+  /** A link to the website icon (should be >= 100x100px) */
+  siteIcon: varchar('siteIcon', { length: 2048 }),
+  imageUrl: varchar('imageUrl', { length: 2048 }),
+  imageTitle: varchar('imageTitle', { length: 1000 }),
+  createdAt: timestamp('createdAt').defaultNow().notNull(),
+  updatedAt: timestamp('updatedAt').defaultNow().notNull(),
+  lastSuccessfulUpd: timestamp('lastSuccessfulUpd').default(new Date(0)).notNull(),
+  lastPubdate: timestamp('lastPubdate').default(new Date(0)),
+
+  // ===
+  /** Activated means that it was added by user with confirmed email */
+  activated: boolean('activated').default(false),
+  lastUpdAttempt: timestamp('lastUpdAttempt').default(new Date(0)).notNull(),
+  throttled: integer('throttled').default(0).notNull(),
+  /* @OneToMany('Item', 'feed', { nullable: true }) */
+  /* items?: IItem[]; */
+});
+
+export const feedRelations = relations(feed, ({ many }) => ({
+  items: many(item),
+}));
+
+export const item = pgTable('item', {
+  id: serial('id').primaryKey(),
+  feedId: integer('feedId').references(() => feed.id, { onDelete: 'cascade' }),
+  guid: varchar('guid', { length: 2048 }),
+  pubdate: timestamp('pubdate'),
+  link: varchar('link', { length: 2048 }),
+  title: varchar('title', { length: 2048 }),
+  description: text('description'),
+  summary: text('summary').default(''),
+  imageUrl: varchar('imageUrl', { length: 2048 }),
+  createdAt: timestamp('createdAt').defaultNow().notNull(),
+});
+
+export const itemsRelations = relations(item, ({ one, many }) => ({
+  feed: one(feed, {
+    fields: [item.feedId],
+    references: [feed.id],
+  }),
+  enclosures: many(enclosure),
+}));
+
+export const enclosure = pgTable('enclosure', {
+  id: serial('id').primaryKey(),
+  url: varchar('url', { length: 2048 }),
+  length: varchar('length', { length: 100 }),
+  type: varchar('type', { length: 20 }),
+  itemId: integer('itemId').references(() => item.id, { onDelete: 'cascade' }),
+});
+
+export const enclosureRelations = relations(enclosure, ({ one }) => ({
+  item: one(item, {
+    fields: [enclosure.id],
+    references: [item.id],
+  }),
+}));
