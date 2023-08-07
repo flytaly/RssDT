a6a5e9f636acc0f269dc69decf34c2f4881c79ca
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-08-07 17:46:20 +0300

    Client fix: Restore Spinner component

--- client/src/components/spinner.tsx
+++ client/src/components/spinner.tsx
@@ -1,7 +1,11 @@
-/* import SpinnerIcon from '../../public/static/spinner.svg'; */
+import SpinnerIcon from '@/../public/static/spinner.svg';
 
 const Spinner = ({ className = '' }: { className?: string }) => {
-  return <span className={className} title="loading"></span>;
+  return (
+    <span className={className} title="loading">
+      <SpinnerIcon className="animate-spin w-4 h-4" />
+    </span>
+  );
 };
 
 export default Spinner;
