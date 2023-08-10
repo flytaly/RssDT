256ea64f6c345fb8d0cb45eb7d8beb6f4da58593
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-08-10 10:35:16 +0300

    Change SVG loader: React-SVG -> SVGR

--- client/next.config.js
+++ client/next.config.js
@@ -1,19 +1,30 @@
-const path = require('path');
-
-/**
- * @type {import('next-react-svg').NextReactSvgConfig}
- */
-const nextReactSvgConfig = {
-  include: path.resolve(__dirname, 'public/static/'),
-};
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ...
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: [{ loader: '@svgr/webpack', options: { dimensions: false } }],
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
-const withReactSvg = require('next-react-svg')(nextReactSvgConfig);
-
-module.exports = withReactSvg(nextConfig);
+module.exports = nextConfig;
