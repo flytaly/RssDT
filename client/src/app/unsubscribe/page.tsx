18ee1608bfa093b1a20157b80985a8e02b12937d
Vitaly Yerofeyevsky <flytaly@gmail.com>
2023-08-20 12:02:15 +0300

    Client Refactor. App router. Unsubscribe page

---
+++ client/src/app/unsubscribe/page.tsx
@@ -0,0 +1,66 @@
+import SmallCard from '@/app/components/card/small-card';
+import { MessageItem } from '@/components/main-card/animated-message';
+import MessagesSide from '@/components/main-card/messages-side';
+
+import Unsubscribing from './unsubscribing';
+import { getGQLClient } from '../lib/gqlClient.server';
+
+const notEnoughInfoMsg: MessageItem = {
+  key: 'query-error',
+  text: 'No token or id provided in the URL',
+  type: 'error',
+  static: true,
+};
+
+function Wrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <SmallCard>
+      <div className="flex flex-col items-center w-full h-full text-center p-4 mt-6">
+        {children}
+      </div>
+    </SmallCard>
+  );
+}
+
+export default async function Unsubscribe({
+  searchParams: { id, token },
+}: {
+  searchParams: { id?: string; token?: string };
+}) {
+  if (!id || !token) {
+    return (
+      <Wrapper>
+        <MessagesSide items={[notEnoughInfoMsg]} />
+      </Wrapper>
+    );
+  }
+
+  const info = await getGQLClient().getFeedInfoByToken({ id, token });
+  if (!info.getFeedInfoByToken?.feed) {
+    return (
+      <Wrapper>
+        <MessagesSide
+          items={[
+            { key: 'query-error', text: 'Could not get informatino about feed', type: 'error' },
+          ]}
+        />
+      </Wrapper>
+    );
+  }
+
+  const { title, url } = info.getFeedInfoByToken.feed;
+
+  return (
+    <Wrapper>
+      <div>
+        <div>Click on the button to unsubscribe from the feed</div>
+        {url ? (
+          <a className="font-bold hover:underline" href={url}>
+            {title || url}
+          </a>
+        ) : null}
+      </div>
+      <Unsubscribing userFeedId={id} token={token} />
+    </Wrapper>
+  );
+}
---
+++ client/src/app/unsubscribe/unsubscribing.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useMutation } from '@tanstack/react-query';
+
+import { MessageItem } from '@/components/main-card/animated-message';
+import MessagesSide from '@/components/main-card/messages-side';
+
+import { getGQLClient } from '../lib/gqlClient.client';
+
+export default function Unsubscribing({
+  token,
+  userFeedId: id,
+}: {
+  token: string;
+  userFeedId: string;
+}) {
+  const { isSuccess, isError, mutate, isLoading, data, error } = useMutation({
+    mutationFn: async () => getGQLClient().unsubscribeByToken({ token, id }),
+  });
+
+  const messages: MessageItem[] = [];
+  if (isSuccess) {
+    if (data?.unsubscribeByToken) {
+      messages.push({ key: 'success', type: 'success', text: 'Successfully unsubscribed' });
+    } else {
+      const text = "Couldn't unsubscribe. Probably wrong token or id";
+      messages.push({ type: 'error', key: 'error_no_success', text });
+    }
+  }
+  if (isError) {
+    messages.push({ type: 'error', text: (error as Error).message, key: 'graphqlError' });
+  }
+  return (
+    <>
+      <button
+        type="submit"
+        className="submit-btn mt-3"
+        onClick={() => {
+          mutate();
+        }}
+        disabled={isLoading || isSuccess}
+      >
+        {isLoading ? 'Unsubscribing' : ' Unsubscribe'}
+      </button>
+      <MessagesSide items={messages} />
+    </>
+  );
+}
