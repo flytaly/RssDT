3ad29ec6517ece027125e5abb0a1902a003631df
Vitaly Yerofeyevsky <flytaly@gmail.com>
2024-08-13 18:42:30 +0300

    Server. fix: pg import

--- server/src/scripts/migrate.ts
+++ server/src/scripts/migrate.ts
@@ -1,13 +1,13 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
-import { Pool } from 'pg';
+import pg from 'pg';
 
 import '#root/dotenv.js';
 import { getPGCredentials } from '#root/pg-connection.js';
 
 console.log('Migration: ', process.env.NODE_ENV || '');
 
-const pool = new Pool(getPGCredentials());
+const pool = new pg.Pool(getPGCredentials());
 
 const db = drizzle(pool);
 
